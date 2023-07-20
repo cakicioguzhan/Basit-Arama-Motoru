822fbef142c50edd8b5f81289706542ed0a5bc37
cakicioguzhan <oguzhankral046@gmail.com>
2023-07-21 00:50:59 +0300

    Bütün gerekli fonksiyonlar yazıldı

--- script.js
+++ script.js
@@ -3,29 +3,50 @@
 // Ürünlerimizi yazalım
 
 let urun1 = {
-    İsim : "Iphone 13 Pro",
-    Kategori : "Telefon",
-    Fiyat : 45.000 + "₺"
+    isim : "Iphone 13 Pro",
+    kategori : "Telefon",
+    fiyat : 45000 + " ₺"
 }
 let urun2 = {
-    İsim : "Xiaomi T13 ",
-    Kategori : "Telefon",
-    Fiyat : 45.000 + "₺"
+    isim : "Xiaomi T13 ",
+    kategori : "Telefon",
+    fiyat : 38000 + " ₺"
 }
 let urun3 = {
-    İsim : "Iphone 14 ",
-    Kategori : "Telefon",
-    Fiyat : 45.000 + "₺"
+    isim : "Iphone 14 ",
+    kategori : "Telefon",
+    fiyat : 52000 + " ₺"
 }
 let urun4 = {
-    İsim : "Iphone 11 Pro Max",
-    Kategori : "Telefon",
-    Fiyat : 45.000 + "₺"
+    isim : "Iphone 11 Pro Max",
+    kategori : "Telefon",
+    fiyat : 23000 + " ₺"
 }
 let urun5 = {
-    İsim : "Xiaomi Note 9 Pro",
-    Kategori : "Telefon",
-    Fiyat : 45.000 + "₺"
+    isim : "Xiaomi Note 9 Pro",
+    kategori : "Telefon",
+    fiyat : 19000 + " ₺"
 }
 
-let ürünler = [urun1,urun2,urun3,urun4,urun5];
+let urunler = [urun1,urun2,urun3,urun4,urun5];
+let filtreliUrunler = [];
+let kullaniciGirilen = prompt("Aramak istediğiniz markayı girin");
+
+filtreliUrunleriDoldur(urunler);
+
+filtreliUrunleriYazdir(filtreliUrunler)
+
+function filtreliUrunleriDoldur(urunler){
+    urunler.forEach(function(urun){
+        if(urun.isim.toUpperCase().includes(kullaniciGirilen.toUpperCase(),0)){
+            filtreliUrunler.push(urun);
+        }
+    })
+}
+
+function filtreliUrunleriYazdir(urunler){
+    urunler.forEach(function(urun){
+        console.log(urun.isim + "|" + urun.kategori + "|" + urun.fiyat);
+        console.log("-------------------------------")
+    })
+}
