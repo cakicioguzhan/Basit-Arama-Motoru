6fe5b0a5ff04242385cc7e86db0fc6a3ac4e59ce
cakicioguzhan <oguzhankral046@gmail.com>
2023-07-20 23:57:16 +0300

    Ürünler yazıldı

---
+++ script.js
@@ -0,0 +1,31 @@
+// Ürün Arama Uygulaması
+
+// Ürünlerimizi yazalım
+
+let urun1 = {
+    İsim : "Iphone 13 Pro",
+    Kategori : "Telefon",
+    Fiyat : 45.000 + "₺"
+}
+let urun2 = {
+    İsim : "Xiaomi T13 ",
+    Kategori : "Telefon",
+    Fiyat : 45.000 + "₺"
+}
+let urun3 = {
+    İsim : "Iphone 14 ",
+    Kategori : "Telefon",
+    Fiyat : 45.000 + "₺"
+}
+let urun4 = {
+    İsim : "Iphone 11 Pro Max",
+    Kategori : "Telefon",
+    Fiyat : 45.000 + "₺"
+}
+let urun5 = {
+    İsim : "Xiaomi Note 9 Pro",
+    Kategori : "Telefon",
+    Fiyat : 45.000 + "₺"
+}
+
+let ürünler = [urun1,urun2,urun3,urun4,urun5];
